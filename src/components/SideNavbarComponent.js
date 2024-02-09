936ba1348797ed552033c3f7031e80e02a85e844
JayvonK <jaykhuth@gmail.com>
2024-02-08 22:12:19 -0800

    FILLING IN SIDE NAV

--- src/components/SideNavbarComponent.js
+++ src/components/SideNavbarComponent.js
@@ -14,55 +14,55 @@ export default function SideNavbarComponent() {
             <div className='navItem '>
                 <Link to={''} className='link d-flex'>
                   <img src='../assets/plus icon.png'/>
-                <h1 className='navTxt'>THE SUM</h1>  
+                <h1 className='navTxt'>NAME & TIME</h1>  
                 </Link>
             </div>
             <div className='navItem '>
                 <Link to={''} className='link d-flex'>
                   <img src='../assets/plus icon.png'/>
-                <h1 className='navTxt'>THE SUM</h1>  
+                <h1 className='navTxt'>GREATER? LESS?</h1>  
                 </Link>
             </div>
             <div className='navItem '>
                 <Link to={''} className='link d-flex'>
                   <img src='../assets/plus icon.png'/>
-                <h1 className='navTxt'>THE SUM</h1>  
+                <h1 className='navTxt'>MAD LIB</h1>  
                 </Link>
             </div>
             <div className='navItem '>
                 <Link to={''} className='link d-flex'>
                   <img src='../assets/plus icon.png'/>
-                <h1 className='navTxt'>THE SUM</h1>  
+                <h1 className='navTxt'>ODD OR EVEN</h1>  
                 </Link>
             </div>
             <div className='navItem '>
                 <Link to={''} className='link d-flex'>
                   <img src='../assets/plus icon.png'/>
-                <h1 className='navTxt'>THE SUM</h1>  
+                <h1 className='navTxt'>REVERSE IT I</h1>  
                 </Link>
             </div>
             <div className='navItem '>
                 <Link to={''} className='link d-flex'>
                   <img src='../assets/plus icon.png'/>
-                <h1 className='navTxt'>THE SUM</h1>  
+                <h1 className='navTxt'>REVERSE IT II</h1>  
                 </Link>
             </div>
             <div className='navItem '>
                 <Link to={''} className='link d-flex'>
                   <img src='../assets/plus icon.png'/>
-                <h1 className='navTxt'>THE SUM</h1>  
+                <h1 className='navTxt'>8 BALL</h1>  
                 </Link>
             </div>
             <div className='navItem '>
                 <Link to={''} className='link d-flex'>
                   <img src='../assets/plus icon.png'/>
-                <h1 className='navTxt'>THE SUM</h1>  
+                <h1 className='navTxt'>RESTAURANTS</h1>  
                 </Link>
             </div>
             <div className='navItem '>
                 <Link to={''} className='link d-flex'>
                   <img src='../assets/plus icon.png'/>
-                <h1 className='navTxt'>THE SUM</h1>  
+                <h1 className='navTxt'>SAY HELLO</h1>  
                 </Link>
             </div>
             </div>
