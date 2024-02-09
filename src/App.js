9bf6645d595bee670293a4f261735403e459beec
JayvonK <jaykhuth@gmail.com>
2024-02-09 01:07:46 -0800

    greaterLess route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SideNavBarComponent from './components/SideNavbarComponent';
 import SumPageComponent from './components/SumPageComponent';
 import HomePageComponent from './components/HomePageComponent';
 import NameTimeComponent from './components/NameTimeComponent';
+import GreaterLessComponent from './components/GreaterLessComponent';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
     <Route path='/' element={<HomePageComponent/>}/>
     <Route path='SumPage' element={<SumPageComponent/>}/>
     <Route path='NameTimePage' element={<NameTimeComponent/>}/>
+    <Route path='GreaterLessPage' element={<GreaterLessComponent/>}/>
 
     </Routes>
     </BrowserRouter>
