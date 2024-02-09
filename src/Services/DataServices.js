092af83a1b31244b0e57cc7eb94360ccba4d3cc8
JayvonK <jaykhuth@gmail.com>
2024-02-09 02:10:55 -0800

    name time page done

--- src/Services/DataServices.js
+++ src/Services/DataServices.js
@@ -4,10 +4,69 @@ const SumApi = async (numOne, numTwo) => {
     return data;
 }
 
+
+const SayHelloApi = async (name) => {
+    const promise = await fetch(`https://khuthjallforone.azurewebsites.net/SayHello/Greeting/${name}`);
+    const data = await promise.text();
+    return data;
+}
+
+
+const Reverse2Api = async (num) => {
+    const promise = await fetch(`https://khuthjallforone.azurewebsites.net/ReverseItNumbers/EnterNumber/${num}`);
+    const data = await promise.text();
+    return data;
+}
+
+
+const Reverse1Api = async (alpha) => {
+    const promise = await fetch(`https://khuthjallforone.azurewebsites.net/ReverseItAlphanumeric/EnterAlphanumeric/${alpha}`);
+    const data = await promise.text();
+    return data;
+}
+
+
+const RestaurantPickerApi = async (category) => {
+    const promise = await fetch(`https://khuthjallforone.azurewebsites.net/RestaurantPicker/Category/${category}`);
+    const data = await promise.text();
+    return data;
+}
+
+
+const OddOrEvenApi = async (num) => {
+    const promise = await fetch(`https://khuthjallforone.azurewebsites.net/OddOrEven/EnterANumber/${num}`);
+    const data = await promise.text();
+    return data;
+}
+
+
+const NameTimeApi = async (name, time) => {
+    const promise = await fetch(`https://khuthjallforone.azurewebsites.net/NameAndTime/EnterYourName/${name}/EnterTimeYouWokeUp/${time}`);
+    const data = await promise.text();
+    return data;
+}
+
+
+const MadLibApi = async (celebrityName, animal, bodyPart, nounThatEndsWithS, wholeNumberGreaterThan1, time, height, adjective, name, condiment) => {
+    const promise = await fetch(`https://khuthjallforone.azurewebsites.net/MadLib/EnterCelebrityName/${celebrityName}/EnterAnimal/${animal}/EnterBodyPart/${bodyPart}/EnterNounThatEndsWithS/${nounThatEndsWithS}/EnterWholeNumberGreaterThan1/${wholeNumberGreaterThan1}/EnterTime/${time}/EnterHeight/${height}/EnterAdjective/${adjective}/EnterName/${name}/EnterCondiment/${condiment}`);
+    const data = await promise.text();
+    return data;
+}
+
+
 const GreaterOrLessApi = async (num1, num2) => {
     const promise = await fetch(`https://khuthjallforone.azurewebsites.net/GreaterOrLessThan/EnterTwoNumbers/${num1}/${num2}`);
     const data = await promise.text();
     return data;
 }
 
-export { SumApi, GreaterOrLessApi }
+
+const EightBallApi = async (question) => {
+    const promise = await fetch(`https://khuthjallforone.azurewebsites.net/EightBall/AskMeYesOrNoQuestion/${question}`);
+    const data = await promise.text();
+    return data;
+}
+
+
+
+export { SumApi, SayHelloApi, EightBallApi, GreaterOrLessApi, MadLibApi, OddOrEvenApi, Reverse1Api, Reverse2Api, RestaurantPickerApi, NameTimeApi }
--- src/components/NameTimeComponent.js
+++ src/components/NameTimeComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { NameTimeApi } from '../Services/DataServices.js';
 
 export default function NameTimeComponent() {
 
     let disappearDiv = document.getElementById("disappearDiv");
     let results = document.getElementById("results");
-    let input1 = document.getElementById("input1");
-    let input2 = document.getElementById("input2");
 
     const [numOne, setNumOne] = useState('');
     const [numTwo, setNumTwo] = useState('');
@@ -17,14 +18,12 @@ export default function NameTimeComponent() {
         if (numOne !== '' && numTwo !== '') {
             if (runBtn === "RUN") {
                 setResult('');
-                const ans = 'await SumApi(numOne, numTwo)';
+                const ans = await NameTimeApi(numOne, numTwo);
                 disappearDiv.classList.add("d-none");
                 results.classList.remove("d-none");
                 setResult(ans);
                 setRunBtn('AGAIN');
             } else {
-                input1.textContent = "";
-                input2.textContent = "";
                 disappearDiv.classList.remove("d-none");
                 results.classList.add("d-none");
                 setRunBtn('RUN');
@@ -34,24 +33,37 @@ export default function NameTimeComponent() {
         }
     }
 
-  return (
-    <div className='homeBg'>
-    <div className='boxDiv'>
-        <div className='apiBox d-flex justify-content-center align-items-center'>
-            <div id='disappearDiv' className='d-flex justify-content-center align-items-center'>
-                <input id='input1' onChange={(e) => setNumOne(e.target.value)} className='inputNums' placeholder='123'></input>
-                <h1 className='plus'>+</h1>
-                <input id='input2' onChange={(e) => setNumTwo(e.target.value)} className='inputNums' placeholder='456'></input>
+    return (
+        <div className='homeBg'>
+            <div className='boxDiv'>
+                <div className='apiBox d-flex justify-content-center align-items-center'>
+                    <div id='disappearDiv' className='w100'>
+                        <Row>
+                            <Col lg={3}>
+                                <p className='nameTxt'>Name:</p>
+                            </Col>
+                            <Col lg={9} className='d-flex align-items-center'>
+                                <input onChange={(e) => setNumOne(e.target.value)} className='nameInput darkBlue jost'></input>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col lg={3}>
+                                <p className='nameTxt'>Time:</p>
+                            </Col>
+                            <Col lg={9} className='d-flex align-items-center'>
+                                <input onChange={(e) => setNumTwo(e.target.value)} className='nameInput darkBlue jost'></input>
+                            </Col>
+                        </Row>
+                    </div>
+
+                    <h1 id='results' className='greaterLessResult d-none'>{result}</h1>
+                </div>
             </div>
 
-            <h1 id='results' className='result d-none'>{result}</h1>
+            <div className='d-flex justify-content-center'>
+                <button id='runBtn' onClick={() => runBtnClick()} className='runBtn'>{runBtn}</button>
+            </div>
+            <p className='darkBlue apiRules'>*Instructions: Input two numbers above to get the sum</p>
         </div>
-    </div>
-
-    <div className='d-flex justify-content-center'>
-        <button id='runBtn' onClick={() => runBtnClick()} className='runBtn'>{runBtn}</button>
-    </div>
-    <p className='darkBlue apiRules'>*Instructions: Input two numbers above to get the sum</p>
-</div>
-  )
+    )
 }
