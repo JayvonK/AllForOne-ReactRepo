1f04c18cf04765d7092e2110895a83ed1b798bf4
JayvonK <jaykhuth@gmail.com>
2024-02-10 21:18:38 -0800

    save

--- src/components/NameTimeComponent.js
+++ src/components/NameTimeComponent.js
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { NameTimeApi } from '../Services/DataServices.js';
-
 export default function NameTimeComponent() {
 
     let disappearDiv = document.getElementById("disappearDiv");
--- src/components/SideNavbarComponent.js
+++ src/components/SideNavbarComponent.js
@@ -16,69 +16,69 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 import mobile from '../assets/mobile icon.png';
 
 export default function SideNavbarComponent() {
+  
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
     <div>
       <div className='sideNav'>
         <div className='itemsDiv'>
-          <div className='navItem '>
+          <div className='navItem'>
             <Link to={'SumPage'} className='link d-flex'>
               <img className='navImg' src={plus} />
               <h1 className='navTxt'>THE SUM</h1>
             </Link>
           </div>
-          <div className='navItem '>
+          <div className='navItem'>
             <Link to={'NameTimePage'} className='link d-flex'>
               <img className='navImg' src={person} />
               <h1 className='navTxt'>NAME & TIME</h1>
             </Link>
           </div>
-          <div className='navItem '>
+          <div className='navItem'>
             <Link to={'GreaterLessPage'} className='link d-flex'>
               <img className='navImg' src={greater} />
               <h1 className='navTxt'>GREATER?</h1>
             </Link>
           </div>
-          <div className='navItem '>
+          <div className='navItem'>
             <Link to={'MadLibPage'} className='link d-flex'>
               <img className='navImg' src={text} />
               <h1 className='navTxt'>MAD LIB</h1>
             </Link>
           </div>
-          <div className='navItem '>
+          <div className='navItem'>
             <Link to={'OddEvenPage'} className='link d-flex'>
               <img className='navImg' src={odd} />
               <h1 className='navTxt'>ODD OR EVEN</h1>
             </Link>
           </div>
-          <div className='navItem '>
+          <div className='navItem'>
             <Link to={'ReverseOnePage'} className='link d-flex'>
               <img className='navImg' src={reverseOne} />
               <h1 className='navTxt'>REVERSE IT I</h1>
             </Link>
           </div>
-          <div className='navItem '>
+          <div className='navItem'>
             <Link to={'ReverseTwoPage'} className='link d-flex'>
               <img className='navImg' src={reverseTwo} />
               <h1 className='navTxt'>REVERSE IT II</h1>
             </Link>
           </div>
-          <div className='navItem '>
+          <div className='navItem'>
             <Link to={'EightBallPage'} className='link d-flex'>
               <img className='navImg' src={eightBall} />
               <h1 className='navTxt'>8 BALL</h1>
             </Link>
           </div>
-          <div className='navItem '>
+          <div className='navItem'>
             <Link to={'RestaurantPage'} className='link d-flex'>
               <img className='navImg' src={food} />
               <h1 className='navTxt'>RESTAURANTS</h1>
             </Link>
           </div>
-          <div className='navItem '>
+          <div className='navItem'>
             <Link to={'SayHelloPage'} className='link d-flex'>
               <img className='navImg' src={hello} />
               <h1 className='navTxt'>SAY HELLO</h1>
